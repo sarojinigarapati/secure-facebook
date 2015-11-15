0131ea1b81dab52f1a8b171876af6ca225bdeb51
sarojini <sarojinigarapati@gmail.com>
2015-11-15 17:07:17 -0500

    50% complete

--- client/client.scala
+++ client/client.scala
@@ -12,23 +12,53 @@ import scala.util.{Success, Failure}
 object project4 {
 	def main(args: Array[String]){
 
-		implicit val system = ActorSystem("ClientSystem")
+		case class Inilialize()
+		case class StartSimulation()
+		case class Post(userID: Int)
 
-		import system.dispatcher
-		val pipeline = sendReceive
-		val responseFuture = pipeline(Get("http://localhost:8080/hello"))
-		responseFuture onComplete {
-			case Success(str: HttpResponse) =>
-				println(str)
-			case Failure(error) =>
-				println(error+"Couldn't get elevation")
-				// shutdown()
+		class ClientMaster(numOfUsers: Int, system: ActorSystem) extends Actor{
+			import system.dispatcher
+			val pipeline = sendReceive
+			
+			def receive = {
+				case Inilialize() =>
+					val responseFuture = pipeline(Post("http://localhost:8080/facebook/start?numOfUsers="+numOfUsers))
+					responseFuture onComplete {
+						case Success(str: HttpResponse) =>
+							println(str)
+							self ! StartSimulation()
+						case Failure(error) =>
+							println(error+"something wrong")
+					}
+
+				case StartSimulation() =>
+					println("Server is initialized! We can start the simulation!")
+			}
 		}
 
-		// def shutdown(): Unit = {
-		// 	IO(Http).ask(Http.CloseAll)(1.second).await
-		// 	system.shutdown()
-		// }
+		class FacebookAPI(system: ActorSystem) extends Actor{
+			import system.dispatcher
+			val pipeline = sendReceive
+
+			def receive = {
+				case Post(userID: Int) =>
+					var text: String = Random.alphanumeric.take(5).mkString
+					pipeline(Post("http://localhost:8080/facebook/Post?userID="+userID+"text"+text))
+
+				case AddFriend(userID:Int) =>
+					pipeline(Post("http://localhost:8080/facebook/addfriend?userID="+userID))
+
+
+			}
+		}
+
+		if(1>args.size){
+			println("Please enter number of facebook users to start simulation!")
+		} else {
+			implicit val system = ActorSystem("ClientSystem")
+			val clientMaster =system.actorOf(Props(new ClientMaster((args(0).toInt),system)),name="clientMaster")
+			clientMaster ! Inilialize()
+		}
 		
 	}	
 }
--- server/server.scala
+++ server/server.scala
@@ -3,47 +3,147 @@ import akka.actor.ActorRef
 import akka.actor.ActorSystem
 import akka.actor.Props
 import akka.routing.RoundRobinRouter
+import scala.util.Random
 import scala.concurrent.duration._
+import scala.collection.mutable._
 import spray.routing.SimpleRoutingApp
 import spray.http._
 
 object project4 extends App with SimpleRoutingApp{
 	override def main(args: Array[String]){
 
-		val numOfServerActors: Int = Runtime.getRuntime().availableProcessors()*100
+		var posts = new ListBuffer[Queue[String]]()
+		var friendLists = new ListBuffer[ListBuffer[Int]]()
+		var profiles = new ListBuffer[Profile]()  // store list of class objects
 
-		case class Hello()
+		case class Init(numOfUsers: Int)
+		case class GetProfile(userID: Int)
+		case class Post(userID: Int, text: String)
+		case class AddFriend(userID: Int)
+		case class UnFriend(userID: Int, firendID: Int)
 
 		println("project4 - Facebook Simulator")
 		class Server(num: Int) extends Actor{
 
 			
 			def receive = {
-				case Hello() =>
-					
+				case Init(numOfUsers: Int) =>
+					println("Initialise profiles for "+numOfUsers+" users!")
+					for(i <- 0 to numOfUsers-1) friendLists += new ListBuffer[Int]
+					for(i <- 0 to numOfUsers-1) posts += new Queue[String]
+					for(i <- 0 to numOfUsers-1){ // Create random profiles
+						var id: Int = i
+						var first_name: String = Random.alphanumeric.take(5).mkString
+						var last_name: String = Random.alphanumeric.take(5).mkString
+						var age: Int = Random.nextInt(82) + 18
+						var email: String = Random.alphanumeric.take(10).mkString
+						var gender: String = ""
+						if(Random.nextInt(1) == 0){
+							gender = "Male"
+						} else {
+							gender = "Female"
+						}
+						var relation_status = ""
+						if(Random.nextInt(1) == 0){
+							relation_status = "Single"
+						} else {
+							relation_status = "Married"
+						}
+						profiles += new Profile(id, first_name, last_name, age, email, gender, relation_status)
+					}
+					// Add some random friends
+					for(i <- 0 to numOfUsers-1){
+						for(j <- 0 to 20){
+							var frd: Int = Random.nextInt(numOfUsers)
+							if(frd != i && !friendLists(i).contains(frd)){
+								friendLists(i) += frd
+							}
+						}
+					}
+
+				case GetProfile(userID: Int) =>
+
+				case Post(userID: Int, text: String) =>
+					posts(userID) += text
+
+				case AddFriend(userID: Int) =>
+					var frd: Int = Random.nextInt(numOfUsers)
+					while(frd == i || friendLists(i).contains(frd)){
+						frd = Random.nextInt(numOfUsers)
+					}
+					friendLists(userID) += frd
+
+				case UnFriend(userID: Int, firendID: Int) =>
+
+
+
 
 			}
 		}
 
+		class Profile(id: Int, first_name: String, last_name: String, age: Int, email: String, gender: String, relation_status: String) {
+			
+		}
+
 		implicit val system = ActorSystem("ServerSystem")
+		val numOfServerActors: Int = Runtime.getRuntime().availableProcessors()*100
 		val server = system.actorOf(Props(new Server(5)).withRouter(RoundRobinRouter(numOfServerActors)), name = "server")
 
 		startServer("localhost", port = 8080) {
 			get {
-				pathSingleSlash {
-					redirect("/hello", StatusCodes.Found)
-				} ~
 				path("hello") {
 					complete {
 						"Hello am there"
 					}
 				}
 			} ~
-			(post | parameter('method ! "post")) {
-				path("stop") {
-					complete {
-						system.scheduler.scheduleOnce(1.second)(system.shutdown())(system.dispatcher)
-						"Shutting down in 1 second..."
+			get {
+				path("facebook"/"getprofile") {
+					parameters("userID".as[Int]) { (userID) =>
+						server ! GetProfile(userID)
+						complete {
+							"Getting profile for user "+userID
+						}
+					}
+				}
+			}~
+			post {
+				path("facebook"/"start") {
+					parameters("numOfUsers".as[Int]) { (numOfUsers) =>
+						server ! Init(numOfUsers)
+						complete {
+							"Initialization is done"
+						}
+					}
+				}
+			}~
+			post {
+				path("facebook"/"post") {
+					parameters("userID".as[Int], "text".as[String]) { (userID, text) =>
+						server ! Post(userID, text)
+						complete {
+							"User "+userID+" posted"
+						}
+					}
+				}
+			}~
+			post {
+				path("facebook"/"addfriend") {
+					parameters("userID".as[Int]) { (userID) =>
+						server ! AddFriend(userID)
+						complete {
+							"Added a friend to user "+userID
+						}
+					}
+				}
+			}~
+			post {
+				path("facebook"/"unfriend") {
+					parameters("userID".as[Int], "firendID".as[Int]) { (userID, firendID) =>
+						server ! UnFriend(userID, firendID)
+						complete {
+							"User "+ userID+" removed "+firendID+" from his/her friend list"
+						}
 					}
 				}
 			}
