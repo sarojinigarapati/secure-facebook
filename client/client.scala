6b911f4ac5d9ea7924004f4def56aab0386b6bee
sharath <sharuu123@gmail.com>
2015-11-30 08:21:15 -0500

    push 7

--- client/client.scala
+++ client/client.scala
@@ -19,51 +19,64 @@ import spray.http.MediaTypes
 object project4 {
 	def main(args: Array[String]){
 
+		val simulationTime: Int = 20000 // in milliseconds
 		val start: Long = System.currentTimeMillis
+		var numOfPages: Int = 0
+
 		case class Stat(userPosts: Int)
 		case class GetProfile(userID: Int)
+		case class GetAlbum(userID: Int, albumID: Int)
 		case class GetPosts(userID: Int)
 		case class GetFriendList(userID: Int)
+
 		case class dummy(first: String, second: String)
 		case class BigDummy(userID: String, d: dummy)
 		case class Profile(id: String, first_name: String, last_name: String, age: String, email: String, gender: String, relation_status: String)
 		case class SignUp(id: Int, profile: Profile)
-		// class Color(val name: String, val red: Int, val green: Int, val blue: Int)
+		case class Picture(id: Int, from: Int, text: String, likes: Array[String])
+		case class Album(id: Int, from: Int, pictures: Array[Picture], likes: Array[String])
+		case class Page(id: Int, from: Int, name: String)
+		// case class AlbumWrapper(userID: Int, albumID: Int,  album: Album)
 
-		class Post(input_id: String, input_message: String, input_from: String, input_to: String, input_taggers: ArrayBuffer[String]) extends java.io.Serializable {
-		   	var id: String = input_id
-		   	var message: String = input_message
-		   	var from: String = input_from
-		   	var to: String = input_to
-		   	var tags: ArrayBuffer[String] = input_taggers
-		}
+		// class Album(input_id: String, input_from: String, input_taggers: ArrayBuffer[String]) extends java.io.Serializable {
+		//    	// Fields
+		//    	var id: String = input_id
+		//    	var from: String = input_from
+		//    	// Edges
+		//    	var likes: ArrayBuffer[String] = input_taggers
+		// }
 
 		object MyJsonProtocol extends DefaultJsonProtocol {
 			implicit val ObjFormat = jsonFormat7(Profile)
 			implicit val SignUpFormat = jsonFormat2(SignUp)
+			implicit val PictureFormat = jsonFormat4(Picture)
+			implicit val AlbumFormat = jsonFormat4(Album)
+			implicit val PageFormat = jsonFormat3(Page)
+			
 			implicit val dummyFormat = jsonFormat2(dummy)
 			implicit val BigDummyFormat = jsonFormat2(BigDummy)
 
-			implicit object PostJsonFormat extends JsonFormat[Post] {
+			// implicit object AlbumJsonFormat extends RootJsonFormat[Album] {
 			   
-			    def write(post: Post) = JsObject(
-			      "id" -> JsString(post.id),
-			      "message" -> JsString(post.message),
-			      "from" -> JsString(post.from),
-			      "to" -> JsString(post.to),
-			      "tags" -> JsArray(post.tags.map(_.toJson).toVector))
+			//     def write(album: Album) = JsObject(
+			//       "id" -> JsString(album.id),
+			//       "from" -> JsString(album.from),
+			//       "likes" -> JsArray(album.likes.map(_.toJson).toVector))
 
-			    def read(value: JsValue) = {
-			      value.asJsObject.getFields("id", "message", "from", "to", "tags") match {
-			        case Seq(JsString(id), JsString(message), JsString(from), JsString(to), JsArray(tags)) =>
-			         	new Post(id, message, from, to, tags.map(_.convertTo[String]).to[ArrayBuffer])
-			       	case Seq(JsString(message), JsString(from), JsArray(tags)) =>
-			        	new Post(null, message, from, null, tags.map(_.convertTo[String]).to[ArrayBuffer])
-			       	case _ =>
-			        	throw new DeserializationException("Invalid post")
-			     	}
-			   }
-			}
+			//     def read(value: JsValue) = {
+			//       value.asJsObject.getFields("id", "from", "likes") match {
+			//         case Seq(JsString(id), JsString(from), JsArray(likes)) =>
+			//          	new Album(id, from, likes.map(_.convertTo[String]).to[ArrayBuffer])
+			//        	case Seq(JsString(message), JsString(from), JsArray(likes)) =>
+			//         	new Album(null, from, likes.map(_.convertTo[String]).to[ArrayBuffer])
+			//        	case _ =>
+			//         	throw new DeserializationException("Invalid Album")
+			//      	}
+			//    }
+			// }
+
+			// implicit val AlbumWrapperFormat = jsonFormat3(AlbumWrapper)
+
 		}
 
 		import MyJsonProtocol._
@@ -83,7 +96,7 @@ object project4 {
 					val responseFuture = pipeline(Post("http://localhost:8080/facebook/start?numOfUsers="+numOfUsers))
 					responseFuture onComplete {
 						case Success(str: HttpResponse) =>
-							println(str.entity.asString)
+							// println(str.entity.asString)
 							self ! "StartSimulation"
 						case Failure(error) =>
 							println(error+"something wrong")
@@ -91,25 +104,23 @@ object project4 {
 				
 				case "StartSimulation" =>
 					// Create an actore for each facebook user
-					println("Server is initialized! We can start the simulation!")
+					println("\n**********************************************")
+					println("Server is initialized! Starting the simulation!")
+					println("Users doing sign up")
+					println("**********************************************\n")
 					for(i <- 0 until numOfUsers){
 						var myID: String = i.toString
 						val actor = context.actorOf(Props(new FacebookAPI(system, i, numOfUsers)),name=myID)
-						println("users doing sign up")
 						actor ! "SignUp"
 						actor ! "Stop"
 					}
 
 				case Stat(userPosts: Int) =>
-					// Number of posts
-					// Number of pictures
-					// Number of albums
-					// Number of pages
 					totalPosts = totalPosts + userPosts
 					count = count + 1
 					if(count == numOfUsers){
 						println("Total number of posts = "+totalPosts)
-						system.scheduler.scheduleOnce(2000 milliseconds, self, "ShutDown")
+						system.scheduler.scheduleOnce(5000 milliseconds, self, "ShutDown")
 					}
 
 				case "ShutDown" =>
@@ -121,11 +132,14 @@ object project4 {
 			extends Actor{
 
 			var numOfPosts: Int =_
+			var numOfAlbums: Int =_
 			// var cancels = new ListBuffer[Cancellable]()
-			var cancelStop: Cancellable =_
 			var cancelPosts1: Cancellable =_
 			var cancelPosts2: Cancellable =_
-			var cancelPosts3: Cancellable =_
+			var cancelAddFriend: Cancellable =_
+			var cancelAddAlbum: Cancellable =_
+			var cancelGetProfile: Cancellable =_
+			var cancelGetAlbum: Cancellable =_
 
 			import system.dispatcher
 			val pipeline = sendReceive
@@ -166,14 +180,18 @@ object project4 {
 					}
 
 				case "DoActivity" =>
-					cancelStop = system.scheduler.schedule(0 milliseconds,100 milliseconds,self,"Stop")
+					system.scheduler.scheduleOnce(simulationTime milliseconds, self, "StopActivity")
+					// cancelStop = system.scheduler.schedule(0 milliseconds,100 milliseconds,self,"Stop")
+					
+					cancelAddFriend = system.scheduler.schedule(0 milliseconds,2000 milliseconds,self,"AddFriend")
+					cancelAddAlbum = system.scheduler.schedule(0 milliseconds,4000 milliseconds,self,"AddAlbum")
+					cancelGetAlbum = system.scheduler.schedule(30 milliseconds,2000 milliseconds,self,GetAlbum(myID, 0))
+					cancelGetProfile = system.scheduler.schedule(60 milliseconds,8000 milliseconds,self,GetProfile(Random.nextInt(numOfUsers)))
 					if(myID % 2 == 0 ){
-						cancelPosts1 = system.scheduler.schedule(0 milliseconds,50 milliseconds,self,"Post")
+						cancelPosts1 = system.scheduler.schedule(150 milliseconds,3000 milliseconds,self,"Post")
 					} else {
-						cancelPosts2 = system.scheduler.schedule(0 milliseconds,100 milliseconds,self,"Post")
+						cancelPosts2 = system.scheduler.schedule(180 milliseconds,6000 milliseconds,self,"Post")
 					}
-					cancelPosts3 = system.scheduler.schedule(0 milliseconds,100 milliseconds,self,"AddFriend")
-
 					// cancels += system.scheduler.schedule(0 milliseconds,100 milliseconds,self,"Stop")
 					// if(myID % 2 == 0 ){
 					// 	cancels += system.scheduler.schedule(0 milliseconds,50 milliseconds,self,"Post")
@@ -182,18 +200,21 @@ object project4 {
 					// }
 					// // cancels += system.scheduler.schedule(0 milliseconds,1000 milliseconds,self,"AddFriend")
 					
-				case "Stop" =>
-					if(System.currentTimeMillis - start > 10000){ // in milliseconds
+				case "StopActivity" =>
+					if(System.currentTimeMillis - start > simulationTime){ // in milliseconds
 						// println("Shutting down the system!!")
-						cancelStop.cancel()
+						// cancelStop.cancel()
+						cancelAddFriend.cancel()
+						cancelAddAlbum.cancel()
+						cancelGetProfile.cancel()
+						cancelGetAlbum.cancel()
 						if(myID % 2 == 0 ){
 							cancelPosts1.cancel()
 						} else {
 							cancelPosts2.cancel()
 						}
-						cancelPosts3.cancel()
 						// self ! GetPosts(myID)
-						self ! GetFriendList(myID)
+						// self ! GetFriendList(myID)
 						context.parent ! Stat(numOfPosts)
 					}
 
@@ -215,7 +236,6 @@ object project4 {
 						case Success(str: HttpResponse) =>
 							println("user "+myID+" received posts of "+userID+"\n"+
 								str.entity.asString)
-								// str.entity.asString.parseJson.convertTo[Profile].age)
 						case Failure(error) =>
 							println(error+"something wrong")
 					}
@@ -225,9 +245,20 @@ object project4 {
 					val responseFuture = pipeline(Post("http://localhost:8080/facebook/getFriendList?userID="+userID))
 					responseFuture onComplete {
 						case Success(str: HttpResponse) =>
-							println("user "+myID+" received posts of "+userID+"\n"+
+							println("user "+myID+" received friend list of "+userID+"\n"+
 								str.entity.asString)
-								// str.entity.asString.parseJson.convertTo[Profile].age)
+						case Failure(error) =>
+							println(error+"something wrong")
+					}
+
+				case GetAlbum(userID: Int, albumID: Int) =>
+					val responseFuture = pipeline(Post("http://localhost:8080/facebook/getAlbum?userID="+userID+"&albumID="+albumID))
+					responseFuture onComplete {
+						case Success(str: HttpResponse) =>
+							val album = str.entity.asString.parseJson.convertTo[Album]
+							println("user "+myID+" received album of "+userID+
+								"\nalbumID "+album.id+
+								"\nlikes "+album.likes.mkString(","))
 						case Failure(error) =>
 							println(error+"something wrong")
 					}
@@ -249,11 +280,58 @@ object project4 {
 					val responseFuture = pipeline(Post("http://localhost:8080/facebook/addFriend?userID="+myID+"&totalUsers="+numOfUsers))
 					responseFuture onComplete {
 						case Success(str: HttpResponse) =>
-							println(str.entity.asString)
+							// println(str.entity.asString)
 						case Failure(error) =>
 							println(error+"something wrong")
 					}
 
+				case "AddPage" =>
+					var id: Int = numOfPages
+					numOfPages = numOfPages + 1
+					var name: String = "page-"+Random.nextInt(numOfUsers).toString
+					val json = Page(id,myID,name).toJson.toString
+					val responseFuture = pipeline(Post("http://localhost:8080/facebook/addPage?pageID=",HttpEntity(MediaTypes.`application/json`, json)))
+					responseFuture onComplete {
+						case Success(str: HttpResponse) =>
+							// println(str.entity.asString)
+						case Failure(error) =>
+							println(error+"something wrong")
+					}
+
+				case "AddAlbum" =>
+					var likes: ArrayBuffer[String] = ArrayBuffer[String](Random.nextInt(numOfUsers).toString)
+					var avgNumOfLikes = Random.nextInt(20)
+					for(i <- 0 to avgNumOfLikes){
+						likes += Random.nextInt(numOfUsers).toString
+					}
+					var picturelikes: ArrayBuffer[String] = ArrayBuffer[String](Random.nextInt(numOfUsers).toString)
+					for(i <- 0 to avgNumOfLikes){
+						likes += Random.nextInt(numOfUsers).toString
+					}
+					var id: Int = Random.nextInt(100)
+					var pictures: ArrayBuffer[Picture] = ArrayBuffer[Picture](Picture(id,myID,("pic-"+Random.nextInt(numOfUsers).toString),likes.toArray))
+					
+					var avgNumOfPictures = Random.nextInt(10)
+					for(i <- 0 to avgNumOfPictures){
+						var picturelikes: ArrayBuffer[String] = ArrayBuffer[String](Random.nextInt(numOfUsers).toString)
+						for(i <- 0 to avgNumOfLikes){
+							likes += Random.nextInt(numOfUsers).toString
+						}
+						var id: Int = Random.nextInt(100)
+						pictures += Picture(id,myID,("pic-"+Random.nextInt(numOfUsers).toString),likes.toArray)
+						
+					}
+					val json = Album(numOfAlbums,myID, pictures.toArray, likes.toArray).toJson.toString
+					// val json = new Album(numOfAlbums.toString,myID.toString,likes).toJson.toString
+					// val json = AlbumWrapper(myID, numOfAlbums, new Album(numOfAlbums.toString,myID.toString,likes)).toJson.toString
+					numOfAlbums = numOfAlbums + 1
+					val responseFuture = pipeline(Post("http://localhost:8080/facebook/addAlbum",HttpEntity(MediaTypes.`application/json`, json)))
+					responseFuture onComplete {
+						case Success(str: HttpResponse) =>
+							// println(str.entity.asString)
+						case Failure(error) =>
+							println(error+"something wrong")
+					}
 			}
 		}
 
--- server/server.scala
+++ server/server.scala
@@ -5,14 +5,11 @@ import akka.actor.Props
 import akka.routing.RoundRobinRouter
 import scala.util.Random
 import scala.concurrent.duration._
-// import scala.collection.immutable.Queue
 import scala.collection.mutable._
 import spray.routing.SimpleRoutingApp
 import spray.http.MediaTypes
 import spray.http._
 import spray.httpx.SprayJsonSupport._
-import spray.httpx.marshalling._
-import spray.httpx.unmarshalling._
 import spray.json._
 import DefaultJsonProtocol._ 
 
@@ -22,30 +19,68 @@ object project4 extends App with SimpleRoutingApp{
 	override def main(args: Array[String]){
 
 		val AvgNumOfFrds: Int = 5
-		var posts = new ListBuffer[Queue[String]]()
-		var friendLists = new ListBuffer[ListBuffer[Int]]()
-		var profiles = new ListBuffer[Profile]()  // store list of class objects
-		var albums = new ListBuffer[ListBuffer[ListBuffer[String]]]
+		var posts = new ArrayBuffer[Queue[String]]()
+		var friendLists = new ArrayBuffer[ListBuffer[Int]]()
+		var profiles = new HashMap[Int, Profile]()  // store list of class objects
+		var albums = new HashMap[Int, HashMap[Int, Album]]()
 
 		case class Init(numOfUsers: Int)
 		case class AddProfile(userID: Int, profile: Profile)
 		case class GetProfile(userID: Int)
 		case class Post(userID: Int, text: String)
 		case class AddFriend(userID: Int, totalUsers: Int)
+		case class AddAlbum(userID: Int, albumID: Int, album: Album) 
 		case class UnFriend(userID: Int, firendID: Int)
+
 		case class dummy(first: String, second: String)
 		case class BigDummy(userID: String, d: dummy)
 		case class Profile(id: String, first_name: String, last_name: String, age: String, email: String, gender: String, relation_status: String)
 		case class SignUp(id: Int, profile: Profile)
+		case class Picture(id: Int, from: Int, text: String, likes: Array[String])
+		case class Album(id: Int, from: Int, pictures: Array[Picture], likes: Array[String])
+		// case class AlbumWrapper(userID: Int, albumID: Int, album: Album)
+
+		// class Album(input_id: String, input_from: String, input_taggers: ArrayBuffer[String]){
+		//    	// Fields
+		//    	var id: String = input_id
+		//    	var from: String = input_from
+		//    	// Edges
+		//    	var likes: ArrayBuffer[String] = input_taggers
+		// }
 
 		object MyJsonProtocol extends DefaultJsonProtocol {
 			implicit val ObjFormat = jsonFormat7(Profile)
 			implicit val SignUpFormat = jsonFormat2(SignUp)
+			implicit val PictureFormat = jsonFormat4(Picture)
+			implicit val AlbumFormat = jsonFormat4(Album)
+
 			implicit val dummyFormat = jsonFormat2(dummy)
 			implicit val BigDummyFormat = jsonFormat2(BigDummy)
+
+			// implicit object AlbumJsonFormat extends RootJsonFormat[Album] {
+			   
+			//     def write(album: Album) = JsObject(
+			//       "id" -> JsString(album.id),
+			//       "from" -> JsString(album.from),
+			//       "likes" -> JsArray(album.likes.map(_.toJson).toVector))
+
+			//     def read(value: JsValue) = {
+			//       value.asJsObject.getFields("id", "from", "likes") match {
+			//         case Seq(JsString(id), JsString(from), JsArray(likes)) =>
+			//          	new Album(id, from, likes.map(_.convertTo[String]).to[ArrayBuffer])
+			//        	case Seq(JsString(message), JsString(from), JsArray(likes)) =>
+			//         	new Album(null, from, likes.map(_.convertTo[String]).to[ArrayBuffer])
+			//        	case _ =>
+			//         	throw new DeserializationException("Invalid Album")
+			//      	}
+			//    }
+			// }
+
+			// implicit val AlbumWrapperFormat = jsonFormat3(AlbumWrapper)
+
 		}
 
-		import MyJsonProtocol._
+		
 
 		println("project4 - Facebook Simulator")
 		class Server(num: Int) extends Actor{
@@ -57,8 +92,9 @@ object project4 extends App with SimpleRoutingApp{
 					println("Initialise profiles for "+numOfUsers+" users!")
 					for(i <- 0 to numOfUsers-1) friendLists += new ListBuffer[Int]
 					for(i <- 0 to numOfUsers-1) posts += new Queue[String]
+					for(i <- 0 to numOfUsers-1) albums(i) = new HashMap[Int,Album]
 					for(i <- 0 to numOfUsers-1){ // Create random profiles
-						profiles += Profile("0", "NA", "NA", "18", "NA", "NA", "NA")
+						profiles(i) = Profile("NA", "NA", "NA", "NA", "NA", "NA", "NA")
 					}
 					Users = numOfUsers
 					println("Initialised for "+Users+" user!!")
@@ -82,15 +118,16 @@ object project4 extends App with SimpleRoutingApp{
 						friendLists(userID) += frd
 					}
 
-				case UnFriend(userID: Int, firendID: Int) =>
-
+				case AddAlbum(userID: Int, albumID: Int, album: Album) =>
+					albums(userID)(albumID) = album
+					// println(albums(userID)(albumID).toString)
 			}
 		}
 
 		implicit val system = ActorSystem("ServerSystem")
 		val numOfServerActors: Int = Runtime.getRuntime().availableProcessors()*100
 		val server = system.actorOf(Props(new Server(5)).withRouter(RoundRobinRouter(numOfServerActors)), name = "server")
-
+		import MyJsonProtocol._
 		startServer("localhost", port = 8080) {
 			get {
 				path("hello") {
@@ -130,12 +167,18 @@ object project4 extends App with SimpleRoutingApp{
 				}
 			}~
 			post {
-				path("dummy"){
-						entity(as[BigDummy]) { d =>
-							complete {
-								d
-							}
+				path("facebook"/"getAlbum") {
+					parameters("userID".as[Int], "albumID".as[Int]) { (userID, albumID) =>
+						// try catch for java.lang.IndexOutOfBoundsException
+						var dummyPic: Picture = Picture(0,0,"dummyPicture",Array("0"))
+						var res: Album = Album(0,0,Array(dummyPic),Array("0"))
+						if((albums(userID).contains(albumID)) == true){
+							res = albums(userID)(albumID)
 						}
+						complete {
+							res
+						}
+					}
 				}
 			}~
 			post {
@@ -151,6 +194,7 @@ object project4 extends App with SimpleRoutingApp{
 			post {
 				path("facebook"/"addProfile"){
 					entity(as[SignUp]) { signUp =>
+						println("Adding a profile!")
 						server ! AddProfile(signUp.id, signUp.profile)
 						complete {
 							"User "+signUp.id+" added"
@@ -158,6 +202,28 @@ object project4 extends App with SimpleRoutingApp{
 					}
 				}
 			}~
+			post {
+				path("facebook"/"addAlbum"){
+					entity(as[Album]) { album =>
+						// println("Adding an album!")
+						server ! AddAlbum(album.from.toInt, album.id.toInt, album)
+						complete {
+							"User "+album.from.toInt+" added an album"
+						}
+					}
+				}
+			}~
+			// post {
+			// 	path("facebook"/"addAlbum"){
+			// 		entity(as[AlbumWrapper]) { albumWrapper =>
+			// 			println("Adding an album!")
+			// 			server ! AddAlbum(albumWrapper.userID, albumWrapper.albumID, albumWrapper.album)
+			// 			complete {
+			// 				"User "+albumWrapper.userID+" added an album"
+			// 			}
+			// 		}
+			// 	}
+			// }~
 			post {
 				path("facebook"/"post") {
 					parameters("userID".as[Int], "text".as[String]) { (userID, text) =>
@@ -174,16 +240,6 @@ object project4 extends App with SimpleRoutingApp{
 						server ! AddFriend(userID, totalUsers)
 						complete {
 							"Added a friend to user "+userID
-						}
-					}
-				}
-			}~
-			post {
-				path("facebook"/"unFriend") {
-					parameters("userID".as[Int], "firendID".as[Int]) { (userID, firendID) =>
-						server ! UnFriend(userID, firendID)
-						complete {
-							"User "+ userID+" removed "+firendID+" from his/her friend list"
 						}
 					}
 				}
